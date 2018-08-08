d867dc5b3c9e1661ff88be111a15ca323cea6742
Sumeet Santani <ssantani@syr.edu>
2018-08-08 17:41:13 -0400

    Update Health Insurance MLR.R

--- Health Insurance MLR.R
+++ Health Insurance MLR.R
@@ -1,13 +1,16 @@
-#Multiple Linear Regression to determine causes behind Death Rates
+#Multiple Linear Regression to determine causes Health Insurance cost predictions
 
-getwd()
+#R Code
 
+getwd() 
+
+#Step 1
 # Importing the dataset
 dataset = read.csv('insurance.csv')
 View(dataset)
 
 
-
+#Step 2
 #Let's encode the categrocial data
 dataset$smoker = factor(dataset$smoker,
                        levels = c('yes', 'no'),
@@ -23,6 +26,7 @@ dataset$region = factor(dataset$region,
                         labels = c(1,2,3,4))
 
 
+#Step 3
 
 # Splitting the dataset into the Training set and Test set
 library(caTools)
@@ -34,11 +38,13 @@ test_set = subset(dataset, split == FALSE)
 View(training_set)
 training_set$
 
+#Step 4
 
 # Fitting Multiple Linear Regression to the Training set
 regressor = lm(formula = charges ~ .,
                data = training_set)
 
+#Step 5
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
@@ -46,16 +52,20 @@ View(test_set)
 
 #Let's find out the more significant independent variables
 
+#Step 6
 summary(regressor)
+#Lower the p-value, higher the statistical significance. More Asterisk means more statistical significance 
+
 
 y_pred
 test_set
 
 
-#Let's remove the independent variables that are have less or no statistical significance
+#Let's remove the independent variables that  have lesser or no statistical significance
 
-#Sex of the person and region don't really play a significant role in determining charges
+#Due to a relatively higher p-value, we can conclude that sex of a person and region don't really play a significant role in determining his/her insurance charges
 
+#Building a new regressor after removing statistically insignificant independent variables
 
 regressorUpdate = lm(formula = charges ~ age + bmi + children + smoker,
                data = training_set)
@@ -63,7 +73,9 @@ summary(regressorUpdate)
 
 test_set
 
+#On comparing the predicted results and the data from the test_set, we can see more accuracy in predictions now
+
 #Insurance Companies check following attributes of a customer in determining premium charges
-#Age 
-#Body Mass Index
-#Smoking habit
+# Age 
+# Body Mass Index
+# Smoking habit
